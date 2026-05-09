e7d8d178a8bbd8a4b839952be7eb122b9e3d6d23
Raghunadha Reddy <rreddy2512@gmail.com>
2026-05-09 12:50:01 +0530

    Delete Backend directory

--- Backend/Model/ContactModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const contactUserSchema = new mongoose.Schema({
-    name : {type:String, required},
-    email : {type:String, required},
-    message :{type:String, required},
-});
-
-export const user = mongoose.model("contactuser",contactUserSchema )
