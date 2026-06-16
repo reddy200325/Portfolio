3787a0b89991f93ce0a99a8be59fd238a943de68
reddy200325 <reddy2512@gmail.com>
2026-06-16 10:15:18 +0530

    case-sensative

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Contact from './components/Contactdetails'
-import Footer from './components/Footer'
-import Work from './components/Work'
-import Services from './components/Service'
-import About from './components/About'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import LenisScroll from './components/LenisScroll'
+import Contact from './Components/Contactdetails'
+import Footer from './Components/Footer'
+import Work from './Components/Work'
+import Services from './Components/Service'
+import About from './Components/About'
+import Header from './Components/Header'
+import Navbar from './Components/Navbar'
+import LenisScroll from './Components/LenisScroll'
 
 export default function App() {
     return (
